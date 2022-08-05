e3ca0cd7766f2aa31a056f401e5c99a91220c429
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-08-05 17:51:13 +0200

    gitting started

---
+++ pages/[meetipId].js
@@ -0,0 +1,15 @@
+// domain/someID
+import MeetupDetail from "../components/meetups/MeetupsDetail";
+const image =
+  "https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000";
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      img={image}
+      titel={"nigga nigga nigga"}
+      address={"nigga nogga nagga"}
+    />
+  );
+};
+
+export default MeetupDetails;
